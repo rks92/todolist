235943827284fe444f3d0cbf6881c1397f31c9fa
rks92 <rahul.shardha@gmail.com>
2020-09-08 21:35:23 -0400

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+3.times do
+  Task.create(
+    name: Faker::Ancient.god,
+    description: Faker::Movie.quote,
+    completed: [true, false].sample
+  )
+end
+
+10.times do
+  p_task = Task.create(
+    name: Faker::House::furniture,
+    description: Faker::Vehicle.model,
+    completed: [true, false].sample
+  )
+
+  rand(1..10).times do 
+    Task.create(
+      name: Faker::House::furniture,
+      description: Faker::Vehicle.model,
+      completed: [true, false].sample,
+      parent: p_task
+    )
+  end
+end
